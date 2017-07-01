77396b7c0e6930c9f605387d41ede3653cfc4736
Geyson D. S. Silva <geysondaniel@gmail.com>
2017-07-01 18:10:03 -0300

    mqtt

--- server.js
+++ server.js
@@ -1,47 +1,37 @@
 const WebSocket = require('ws');
-var express = require('express');
-var app = express();
+var mqtt = require('mqtt')
 
-var bodyParser = require('body-parser');
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
 
-// Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+client.on('connect', function () {
+ 
+})
 
-const wss = new WebSocket.Server({ port: 3000, path: '/room' });
+client.on('message', function (topic, message) {
+ // message is Buffer 
+ wss.clients.forEach(function each(c) {
+		if (c.readyState === WebSocket.OPEN) {
+			c.send("mqtt;" + topic + ";" + message.toString());
+		}
+	});
+ console.log()
+})
 
-var users = [];
+const wss = new WebSocket.Server({ port: 3000, path: '/splash' });
 
 wss.on('connection', function(ws) {
-	
-	ws.on('message', function(message) {
-		console.log('Msg received in server: %s ', message);
-		// Broadcast to everyone else.
-		wss.clients.forEach(function each(client) {
-			if (client.readyState === WebSocket.OPEN) {
-				client.send(message);
-			}
-		});
-		
-	});
+  ws.on('message', function(message) {
 
-	console.log('new connection');
+  	if (message.includes('sitf-subscribe')) {
+  		var protocol = message.split(";");
+  		client.subscribe(protocol[1]);
+  	} else if (message.includes('sitf-publish')) {
+  		var protocol = message.split(";");
+  		client.publish(protocol[1], protocol[2]);
+  	}
+
+    console.log('received: %s', message);
+  });
+
+  console.log('new connection');
 });
-
-function User(name) {
-	this.name = name;
-}
-
-app.get('/discussions', function (req, res) {
-	res.sendFile( __dirname + "/" + "login.html" );
-})
-
-app.post('/discussions', urlencodedParser, function (req, res) {
-	console.log(req.body.nickname);
-	res.sendFile( __dirname + "/" + "discussions.html" );
-})
-
-var server = app.listen(8081, function () {
-	var host = server.address().address
-	var port = server.address().port
-	console.log("Example app listening at http://%s:%s", host, port)
-})

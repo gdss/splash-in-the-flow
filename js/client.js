21893053f86448b23e56fd69c532947193afe52e
Geyson D. S. Silva <geysondaniel@gmail.com>
2017-07-02 19:54:24 -0300

    removendo room

--- js/client.js
+++ js/client.js
@@ -1,11 +1,95 @@
-const WebSocket = require('ws');
+var ws = new WebSocket('ws://192.168.0.150:3000/splash');
 
-const ws = new WebSocket('ws://localhost:3000/splash');
+var nickname;
+var topic;
 
-ws.on('open', function open() {
-  ws.send('something');
+ws.onopen = function() {
+  //ws.send('something');
+}
+
+ws.onmessage = function(e) {
+  if (e.data.includes('mqtt')) {
+     var protocol = e.data.split(";");
+     if (topic == protocol[1]) {
+        addMessageToChat(protocol[2]);
+        updateScroll();
+     }
+  } else if(e.data.includes('sitf-topics-return')) {
+
+     document.getElementById("discussions-list").innerHTML = '';
+   
+      var protocol = e.data.split(";");
+      protocol.shift();
+      protocol = protocol.toString().split(',');
+
+      for (var i = 0; i < protocol.length; i++) {
+        if (!protocol[i] == '') {
+           document.getElementById("discussions-list").innerHTML += '<a href="#!" class="collection-item"><span class="new badge" data-badge-caption="">SI</span><span class="discussion-topic">' + protocol[i] + '</span></a>'
+        }
+      }
+
+        $('.collection-item').click(function() {
+           goRoom($(this).children().eq(1).text());
+        });
+
+  } else if (e.data.includes('sitf-login-sucess')) {
+  	goDiscussions();
+  } else if (e.data.includes('sitf-login-failure')) {
+  	alert('O login já está em uso ou está inválido');
+  }
+}
+
+function addMessageToChat(message) {
+  document.getElementById("room-chat").innerHTML += message + "<br>";
+}
+
+ws.onclose = function() {
+  alert('close');
+}
+
+$('#login-start').click(function() {
+  nickname = $('#login-input-nick').val();
+  ws.send('sitf-login;' + nickname);
 });
 
-ws.on('message', function incoming(data) {
-  console.log(data);
+$('#new-discussion-submit').click(function() {
+  var title = $('#new-discussion-title').val();
+  goRoom(title);
+});
+
+function goDiscussions() {
+  ws.send("sitf-topics");
+  $('#splash-login').css('display', 'none');
+  $('#splash-discussions').css('display', 'block');
+  $('#splash-room').css('display', 'none');
+}
+
+function goRoom(t) {
+  topic = 'sitf-' + t;
+  var protocol = "sitf-subscribe;" + topic;
+  ws.send(protocol);
+  $('#splash-discussions').css('display', 'none');
+  $('#splash-room').css('display', 'block');
+  $('#room-title').text(t);
+
+  document.getElementById("room-chat").innerHTML = '';
+  ws.send('sitf-publish;'+ topic + ';' + nickname + ' entrou na sala');
+}
+
+function sendMessage(event) {
+  var x = event.keyCode;
+  if (x == 13) {
+     ws.send('sitf-publish;'+ topic + ';' + nickname + ': ' + $('#room-msg').val());
+     $('#room-msg').val("");
+  }
+}
+
+function updateScroll(){
+	var element = document.getElementById("room-chat");
+	element.scrollTop = element.scrollHeight;
+}
+
+$('#room-exit').click(function() {
+	ws.send('sitf-exit-subscribe;' + topic);
+	goDiscussions();
 });
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const WebSocket = require('ws');
 var mqtt = require('mqtt');
 
 var client  = mqtt.connect('mqtt://test.mosquitto.org');
-var topics = [];
+var topics = {};
+var logins = [];
 
 client.on('connect', function () {
 
@@ -26,23 +27,35 @@ wss.on('connection', function(ws) {
   		var protocol = message.split(";");
   		client.subscribe(protocol[1]);
   		var topic = protocol[1].substring(5);
-  		if (!topics.includes(topic)) {
-  			topics.push(topic);
- 
-  			 wss.clients.forEach(function each(c) {
-				if (c.readyState === WebSocket.OPEN) {
-					c.send('sitf-topics-return;' + topics);
-				}
-			});
+
+  		if (Object.keys(topics).includes(topic)) {
+  			topics[topic] += 1;
+  		} else {
+  			topics[topic] = 1;
+  			attTopics();
   		}
+  		console.log('o topico ' + topic + ' tem ' + topics[topic]);
   	} else if (message.includes('sitf-publish')) {
   		var protocol = message.split(";");
   		client.publish(protocol[1], protocol[2]);
   	} else if (message.includes('sitf-topics')) {
-  		ws.send('sitf-topics-return;' + topics);
+  		ws.send('sitf-topics-return;' + Object.keys(topics));
   	} else if (message.includes('sitf-login')) {
-  		ws['login'] = message.split(";")[1];
-  		ws.send('sitf-login-return');
+  		var login = message.split(";")[1];
+  		if (!logins.includes(login) && login != '') {
+  			logins.push(login);
+  			ws.send('sitf-login-sucess');
+  		} else {
+  			ws.send('sitf-login-failure');
+  		}
+  	} else if (message.includes('sitf-exit-subscribe')) {
+  		var topic = message.split(";")[1].substring(5);
+  		topics[topic] -= 1;
+  		console.log('o topico ' + topic + ' tem ' + topics[topic]);
+  		if (topics[topic] <= 0) {
+  			delete topics[topic];
+  			attTopics();
+  		}
   	}
 
     console.log('received: %s', message);
@@ -50,3 +63,11 @@ wss.on('connection', function(ws) {
 
   console.log('new connection');
 });
+
+function attTopics() {
+	wss.clients.forEach(function each(c) {
+		if (c.readyState === WebSocket.OPEN) {
+			c.send('sitf-topics-return;' + Object.keys(topics));
+		}
+	});
+}
